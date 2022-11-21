19378b5be360ab84cdc51c5b59a850c6d620a4b1
vitorribeiro99 <v99ribeiro@gmail.com>
2022-11-21 22:08:24 +0000

    add: nft collection

--- Whitelist-Dapp/hardhat-tutorial/contracts/Whitelist.sol
+++ Whitelist-Dapp/hardhat-tutorial/contracts/Whitelist.sol
@@ -1,29 +1,36 @@
-// SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+
 
 contract Whitelist {
 
-    // maximum number of addresses which can be whitelisted
-    uint8 public maxWhitelistAddresses;
-    
-    //keep track of number of addresses whitelisted till now
-    uint8 public numAddressWhitelisted;
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
 
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false my default for all other addresses.
     mapping(address => bool) public whitelistedAddresses;
 
-    constructor(uint8 _maxWhitelistAddresses) {
-        maxWhitelistAddresses = _maxWhitelistAddresses;
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    uint8 public numAddressesWhitelisted;
+
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
     }
 
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
     function addAddressToWhitelist() public {
-        require(!whitelistedAddresses[msg.sender], "Sender already in the whitelist");
-        require(numAddressWhitelisted < maxWhitelistAddresses, "Max limit reached");
-
+        // check if the user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        // Add the address which called the function to the whitelistedAddress array
         whitelistedAddresses[msg.sender] = true;
-        numAddressWhitelisted += 1;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
     }
-
-    
-
 
 }
