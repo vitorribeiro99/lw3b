563bd85592727b9c691bc94af48435a0a20e6190
vitorribeiro99 <v99ribeiro@gmail.com>
2022-11-21 22:25:10 +0000

    new smart contract address

--- Whitelist-Dapp/NFT-Collection/hardhat-tutorial/constants/index.js
+++ Whitelist-Dapp/NFT-Collection/hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xec80021700a11cD26BC59a89df63d5E17B8A0371";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "nft-collection-2g3geqhr0-vitorribeiro99.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
